aa741dc3af99f8607b6a09acec9a41d74f2ec6a6
muhammad syahmi <syahmiez96@gmail.com>
2024-04-25 17:08:54 +0800

    editing landing page

--- src/app/components/season/Season.jsx
+++ src/app/components/season/Season.jsx
@@ -4,9 +4,7 @@ import Image from 'next/image';
 import axios from 'axios'
 const Season = () => {
 
-
   const [season, setSeason]=useState([]);
-
   const getSeasonInfo=async()=>
   {
     try{
@@ -19,7 +17,6 @@ const Season = () => {
         const data=response.data;
         arrayData.push(data)
         const chosenData=(arrayData[0].data[32]); 
-        // console.log(chosenData);
         setSeason(chosenData);
 
     }   
@@ -38,12 +35,9 @@ const Season = () => {
 
   return (
     <section className="flex relative w-full h-100 justify-center items-center ">
-        <Image src={`https://pbs.twimg.com/media/GJcn6vUXUAEwtsZ.jpg:large`}  width={2000} height={2000} className="w-full block
-        " alt=""/>
+        <Image src={`https://pbs.twimg.com/media/GJcn6vUXUAEwtsZ.jpg:large`}  width={2000} height={2000} className="w-full block" alt=""/>
             {season &&
-
                     <div className=" absolute flex flex-row justify-center mx-10">
-                   
                         <div className="bg-[#DCD9D4] md:p-6  p-3  rounded bg-opacity-90">
                              <p className='text-center md:text-3xl  font-bold text-[#ff4656]'>// SEASON // </p>
                              <h2 className="text-[#ff4656] text-center font-bold text-3xl md:text-5xl ">{season.displayName }</h2>
@@ -57,4 +51,4 @@ const Season = () => {
   )
 }
 
-export default Season
+export default Season;
